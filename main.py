aec7558b8997137fb23fc1411893e9f6520aa309
Marissa Hugill <121797316+hugilldev@users.noreply.github.com>
2025-04-27 17:29:29 +0100

    Change port

--- main.py
+++ main.py
@@ -69,4 +69,4 @@ async def startup_event():
 
 # Main entry point to start the server
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=80)
